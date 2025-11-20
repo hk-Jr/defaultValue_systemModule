89e91ae940062876324c378f67c3d4a0bba0f3a4
Krishna H K <khk803123@gmail.com>
2025-11-20 16:31:28 +0530

    Add user input handling for script parameters

---
+++ default.py
@@ -0,0 +1,14 @@
+import sys
+if len(sys.argv)==3:
+  script_name=sys.argv[0]
+  name=sys.argv[1]
+  rollNo=argv[2]
+  print(f"\n\nUser details provided!!!")
+
+else:
+  script_name=sys.argv[0]
+  name="king"
+  rollNo=157
+  print("no input given - Using default Value.")
+
+print(f"Script name:{script_name}\nName:{name}\nRoll No:{rollNo}.")
